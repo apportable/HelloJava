80014a0e8ae7af8709f3fcc73882c186b5041f62
Oleg Osin <oleg@apportable.com>
2015-03-02 16:55:05 +0700

    Changed incorrect usage of BK2JavaObject in the bridge.

--- HelloJava/HelloBridge.m
+++ HelloJava/HelloBridge.m
@@ -16,29 +16,29 @@
     // even though that this is a static method (this preserves inheritance
     // to the correct java class
     [HelloBridge registerConstructorWithSelector:@selector(initWithIntValue:doubleValue:)
-                                       arguments:[BK2JavaObject intPrimitive], [BK2JavaObject doublePrimitive], nil];
+                                       arguments:[BK2JavaClass intPrimitive], [BK2JavaClass doublePrimitive], nil];
     
     [HelloBridge registerInstanceMethod:@"getIntValue"
                                selector:@selector(intValue)
-                            returnValue:[BK2JavaObject intPrimitive]];
+                            returnValue:[BK2JavaClass intPrimitive]];
 
     [HelloBridge registerInstanceMethod:@"setIntValue"
                                selector:@selector(setIntValue:)
-                            arguments:[BK2JavaObject intPrimitive], nil];
+                            arguments:[BK2JavaClass intPrimitive], nil];
 
     [HelloBridge registerInstanceMethod:@"getDoubleValue"
                                selector:@selector(doubleValue)
-                            returnValue:[BK2JavaObject doublePrimitive]];
+                            returnValue:[BK2JavaClass doublePrimitive]];
 
     [HelloBridge registerInstanceMethod:@"setDoubleValue"
                                selector:@selector(setDoubleValue:)
-                            arguments:[BK2JavaObject doublePrimitive], nil];
+                            arguments:[BK2JavaClass doublePrimitive], nil];
 
 
     [HelloBridge registerCallback:@"bridgeCallback"
                          selector:@selector(valueChanged:doubleValue:)
                       returnValue:nil
-                        arguments:[BK2JavaObject intPrimitive], [BK2JavaObject doublePrimitive], nil];
+                        arguments:[BK2JavaClass intPrimitive], [BK2JavaClass doublePrimitive], nil];
 }
 
 + (NSString *)className
